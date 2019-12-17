e6b75ebb4c8e357ae7895cfe6eac55909037fdac
NerminYelboga <nermin.yelboga@gmail.com>
2019-12-17 09:28:43 -0800

    Added types of functions

---
+++ JAvaScriptFunction.js
@@ -0,0 +1,18 @@
+
+
+
+//type of functions
+
+function getName(params){
+    //void functions
+}
+
+function getArgs(args){
+    return args
+    //this is functions with return type
+
+}
+
+const someName= (firstName, lasName)=>{
+    return firstName+lasName;
+}
